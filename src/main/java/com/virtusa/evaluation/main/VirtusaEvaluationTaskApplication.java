38f8d7ec96b02ce0624661f57cfb901e3a04ea6a
GiRi SaHaa <giri91sa@gmail.com>
2019-03-28 20:15:21 +0530

    Initial commit

---
+++ src/main/java/com/virtusa/evaluation/main/VirtusaEvaluationTaskApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.evaluation.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtusaEvaluationTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtusaEvaluationTaskApplication.class, args);
+	}
+
+}
